4a0457048bf5c476d8de1f93bfea2b755a727263
bahelit <salmons.mike@gmail.com>
2024-07-16 10:25:57 -0400

    Add build tags. go mod tidy.

--- pkg/clipboard/clipboard_unix.go
+++ pkg/clipboard/clipboard_unix.go
@@ -3,6 +3,7 @@
 // license that can be found in the LICENSE file.
 
 //go:build freebsd || linux || netbsd || openbsd || solaris || dragonfly
+// +build freebsd linux netbsd openbsd solaris dragonfly
 
 package clipboard
 
